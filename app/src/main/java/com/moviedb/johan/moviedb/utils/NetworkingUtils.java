6a671ecde571d5866973f4aaf27b513f176ead2a
Johan Van Beusekom <johan.beusekom@gmail.com>
2015-09-17 13:07:36 -0400

    baseImageUrl fixed

--- app/src/main/java/com/moviedb/johan/moviedb/utils/NetworkingUtils.java
+++ app/src/main/java/com/moviedb/johan/moviedb/utils/NetworkingUtils.java
@@ -7,7 +7,7 @@ public class NetworkingUtils {
 
     public static String baseUrl = "https://api.themoviedb.org/3";
     public static String apiKey = "8b1ce0834d60ca35748781fb6b8c8331";
-    public static String baseImageUrl = "http://image.tmdb.org/t/p/";
+    public static String baseImageUrl = "http://image.tmdb.org/t/p/w500";
 
 
 }
